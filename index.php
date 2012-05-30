0b0aa5d05042c9071c06c9db47c1e985c81e8492
Stephan Hagedorn <klarsichtweise@googlemail.com>
2012-05-30 14:37:58 +0200

    Preparation for style overhaul
    Renamed menu items.
    Added some ids and classes.

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ $order = set_get_order ();
 <h1 id="caption"><?php echo $username; ?>&#039;s Online Bookmarks</h1>
 
 <!-- Wrapper starts here. -->
-<div style="min-width: <?php echo 230 + $settings['column_width_folder']; ?>px;">
+<div id="root" style="min-width: <?php echo 230 + $settings['column_width_folder']; ?>px;">
 	<!-- google search box -->
 	<div class="desktop" id="googlesearch">
 		<form method="get" action="http://www.google.com/search" target="_blank">
@@ -123,37 +123,37 @@ $order = set_get_order ();
 			<?php if ($search_mode) { ?>
 			<li><a href="./index.php"><?php echo $settings['root_folder_name']; ?></a></li>
 			<?php } ?>
-		  <li><a href="javascript:bookmarknew('<?php echo $folderid; ?>')">New Bookmark</a></li>
-		  <li><a href="javascript:bookmarkedit(checkselected())">Edit Bookmarks</a></li>
-		  <li><a href="javascript:bookmarkmove(checkselected())">Move Bookmarks</a></li>
-		  <li><a href="javascript:bookmarkdelete(checkselected())">Delete Bookmarks</a></li>
-		  <li><a href="./shared.php">Shared Bookmarks</a></li>
+		  <li><a class="menuLink" href="javascript:bookmarknew('<?php echo $folderid; ?>')">Create New</a></li>
+		  <li><a class="menuLink" href="javascript:bookmarkedit(checkselected())">Edit</a></li>
+		  <li><a class="menuLink" href="javascript:bookmarkmove(checkselected())">Move</a></li>
+		  <li><a class="menuLink" href="javascript:bookmarkdelete(checkselected())">Delete</a></li>
+		  <li><a class="menuLink" href="./shared.php">Show Shared</a></li>
 		</ul>
 		</div>
 	
 		<div class="navblock">
 		<h2 class="nav mnu" target="mnu_folders">Folders</h2>
 		<ul class="nav" id="mnu_folders">
-			<li><a href="javascript:foldernew('<?php echo $folderid; ?>')">New Folder</a></li>
-			<li><a href="javascript:folderedit('<?php echo $folderid; ?>')">Edit Folder</a></li>
-			<li><a href="javascript:foldermove('<?php echo $folderid; ?>')">Move Folder</a></li>
-			<li><a href="javascript:folderdelete('<?php echo $folderid; ?>')">Delete Folder</a></li>
-			<li><a href="./index.php?expand=&amp;folderid=0">Collapse All</a></li>
+			<li><a class="menuLink" href="javascript:foldernew('<?php echo $folderid; ?>')">Create New</a></li>
+			<li><a class="menuLink" href="javascript:folderedit('<?php echo $folderid; ?>')">Edit</a></li>
+			<li><a class="menuLink" href="javascript:foldermove('<?php echo $folderid; ?>')">Move</a></li>
+			<li><a class="menuLink" href="javascript:folderdelete('<?php echo $folderid; ?>')">Delete</a></li>
+			<li><a class="menuLink" href="./index.php?expand=&amp;folderid=0">Collapse All</a></li>
 		</ul>
 		</div>
 		
 		<div class="navblock">
 		<h2 class="nav mnu" target="mnu_tools">Tools</h2>
-		<ul class="nav" id="mnu_tools">
+		<ul class="nav lastItem" id="mnu_tools">
 			<?php if (admin_only ()) { ?>
-			<li><a href="./admin.php">Admin</a></li>
+			<li><a class="menuLink" href="./admin.php">Admin</a></li>
 			<?php } ?>
-			<li><a href="./import.php">Import</a></li>
-			<li><a href="./export.php">Export</a></li>
-			<li><a href="./index.php?search=[dupe_check_bookmarks]">Find Duplicates</a></li>
-			<li><a href="./sidebar.php">View as Sidebar</a></li>
-			<li><a href="./settings.php">Settings</a></li>
-			<li><a href="./index.php?logout=1">Logout</a></li>
+			<li><a class="menuLink" href="./settings.php">Settings</a></li>
+			<li><a class="menuLink" href="./import.php">Import</a></li>
+			<li><a class="menuLink" href="./export.php">Export</a></li>
+			<li><a class="menuLink" href="./index.php?search=[dupe_check_bookmarks]">Find Duplicates</a></li>
+			<li><a class="menuLink" href="./sidebar.php">Sidebar</a></li>
+			<li><a class="menuLink" href="./index.php?logout=1">Logout</a></li>
 		</ul>
 		</div>
 	<!-- Menu ends here. -->
@@ -222,7 +222,7 @@ $order = set_get_order ();
 
 	<!-- Folders starts here. -->
 	<h2 id="folders-head" class="mobile nav mnu" target="folders">Folders</h2>
-	<div id="folders" class="folders mnu <?php echo (is_mobile_browser() ? "mobile" : ""); ?>" style="width: <?php echo ($column_width_folder == 0) ? "auto" : $column_width_folder; ?>; height: <?php echo ($table_height == 0) ? "auto" : $table_height; ?>;">
+	<div id="folders" class="folders mnu <?php echo (is_mobile_browser() ? "mobile" : ""); ?>" style="width: <?php echo ($column_width_folder == 0) ? "auto" : $column_width_folder; ?>; height: <?php echo ($table_height == 0) ? "auto" : (($table_height == 1) ? "100%" : $table_height); ?>;">
 
 	<?php
 	require_once (ABSOLUTE_PATH . "folders.php");
@@ -235,7 +235,7 @@ $order = set_get_order ();
 	</div>
 
 	<!-- Bookmarks starts here. -->
-	<div class="bookmarks" style="height: <?php echo ($table_height == 0) ? "auto" : $table_height; ?>;">
+	<div class="bookmarks" style="height: <?php echo ($table_height == 0) ? "auto" : (($table_height == 1) ? "100%" : $table_height); ?>;">
 
 	<?php
 
